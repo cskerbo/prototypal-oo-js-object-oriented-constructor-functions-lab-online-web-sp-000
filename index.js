14e26d8d3eaa6298a0b80caddc23df20728d3ce1
Chris Kerbo <chris.kerbo@outlook.com>
2020-03-31 04:40:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-function Scooter(year, color, model) (
+function Scooter(year, color, model) {
   return {
     year,
     color,
     model
   }
-)
+}
+
+function Driver(name, age, experience) {
+  return {
+    name,
+    age,
+    experience
+  }
+}
